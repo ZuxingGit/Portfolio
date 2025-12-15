2ddfd180ecba64343dd89e659e11d513c14b09fa
Zuxing_a1816653 <a1816653@adelaide.edu.au>
2025-12-15 11:27:03 +1030

    border transparency

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -62,7 +62,7 @@ const Contact = () => {
         refresh
       />
       {showAlert && <Alert type={alertType} text={alertMessage} />}
-      <div className='flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary'>
+      <div className='flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/30 rounded-2xl bg-primary'>
         <div className='flex flex-col items-start w-full gap-5 mb-10'>
           <h2 className='text-heading'>Contact Me</h2>
           <p className='font-normal text-neutral-300'>
